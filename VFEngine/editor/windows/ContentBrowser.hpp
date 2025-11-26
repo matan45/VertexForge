9a743e13dad574c824868b8e13ce7d41819eefc5
matan <matan33214@gmail.com>
2025-11-26 20:28:45 +0200

    update

--- VFEngine/editor/windows/ContentBrowser.hpp
+++ VFEngine/editor/windows/ContentBrowser.hpp
@@ -24,7 +24,7 @@ namespace windows
     {
     private:
         std::vector<Asset> assets;
-        fs::path currentPath = "d:\\matan"; //todo get this path from the project file
+        fs::path currentPath = "D:\\matan"; //todo get this path from the project file
         std::string searchQuery;
 
         std::string newFolderName;
