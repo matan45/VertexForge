2a6e5d1675fffc6cad17a43503200a00bf149504
matan45 <matan33214@gmail.com>
2025-12-09 10:03:04 +0200

    json

--- VFEngine/editor/windows/SceneGraph.hpp
+++ VFEngine/editor/windows/SceneGraph.hpp
@@ -2,21 +2,22 @@
 #include "imguiHandler/ImguiWindow.hpp"
 #include "data/EntityHandle.hpp"
 
-namespace windows {
-	class SceneGraph : public controllers::imguiHandler::ImguiWindow
-	{
-	private:
-		services::EntityHandle selectedHandle;
+namespace windows
+{
+    class SceneGraph : public controllers::imguiHandler::ImguiWindow
+    {
+    private:
+        services::EntityHandle selectedHandle;
 
-	public:
-		SceneGraph() = default;
-		~SceneGraph() override = default;
+    public:
+        SceneGraph() = default;
+        ~SceneGraph() override = default;
 
-		void draw() override;
+        void draw() override;
 
-	private:
-		void drawEntityNode(services::EntityHandle handle);
-		void drawDetails(services::EntityHandle handle);
-		void dragDropEntity(services::EntityHandle handle);
-	};
+    private:
+        void drawEntityNode(services::EntityHandle handle);
+        void drawDetails(services::EntityHandle handle);
+        void dragDropEntity(services::EntityHandle handle);
+    };
 }
