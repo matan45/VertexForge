80310eb69486e4ac474aaace4230d1ebaedf195f
matan45 <matan33214@gmail.com>
2025-12-14 06:31:45 +0200

    imgui node

--- premake5.lua
+++ premake5.lua
@@ -124,11 +124,11 @@ project "Import"
       defines { "DEBUG" }
       symbols "On"
       libdirs { "dependencies/assimp/lib/Debug" }
-      links { "assimp-vc145-mtd.lib" }  -- Assimp Debug library
+      links { "assimp-vc143-mtd.lib" }  -- Assimp Debug library
 
     -- Copy the DLL to the Editor's output directory after the build
    postbuildcommands {
-      "{COPY} ../../dependencies/assimp/bin/Debug/assimp-vc145-mtd.dll ../../bin/Editor/Debug/x64/"
+      "{COPY} ../../dependencies/assimp/bin/Debug/assimp-vc143-mtd.dll ../../bin/Editor/Debug/x64/"
    }
 
    -- Release configuration
@@ -136,11 +136,11 @@ project "Import"
       defines { "NDEBUG" }
       optimize "On"
       libdirs { "dependencies/assimp/lib/Release" }
-      links { "assimp-vc145-mt.lib" }  -- Assimp Release library
+      links { "assimp-vc143-mt.lib" }  -- Assimp Release library
 
       -- Copy the DLL to the output directory after the build
       postbuildcommands {
-         "{COPY} ../../dependencies/assimp/bin/Release/assimp-vc145-mt.dll ../../bin/Editor/Release/x64/"
+         "{COPY} ../../dependencies/assimp/bin/Release/assimp-vc143-mt.dll ../../bin/Editor/Release/x64/"
       }
 
 
