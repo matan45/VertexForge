8c084a977b9480485cd31b0d75118af241b92fed
matan <matan33214@gmail.com>
2025-12-31 11:29:14 +0200

    open

--- VFEngine/editor/handlers/EditorHandler.cpp
+++ VFEngine/editor/handlers/EditorHandler.cpp
@@ -14,141 +14,153 @@
 #include "Import.hpp"
 #include "print/EditorLogger.hpp"
 
-namespace handlers {
-	EditorHandler::EditorHandler()
-		: bootstrap{ std::make_unique<core::EditorBootstrap>() }
-		, windowImguiHandler{ std::make_unique<WindowImguiHandler>() }
-	{
-	}
+namespace handlers
+{
+    EditorHandler::EditorHandler()
+        : bootstrap{std::make_unique<core::EditorBootstrap>()}
+          , windowImguiHandler{std::make_unique<WindowImguiHandler>()}
+    {
+    }
 
-	EditorHandler::~EditorHandler() = default;
+    EditorHandler::~EditorHandler() = default;
 
 
-	void EditorHandler::init()
-	{
-		bootstrap->init();
-		
-		controllers::Import::initialize();
+    void EditorHandler::init()
+    {
+        bootstrap->init();
 
-		// Initialize services with providers from bootstrap
-		initializeServices();
+        controllers::Import::initialize();
 
-		// Set up frame callback to update services each frame
-		bootstrap->setFrameCallback([this]() {
-			if (inputService) {
-				inputService->update();
-			}
-			if (windowStateService) {
-				windowStateService->update();
-			}
-		});
+        // Initialize services with providers from bootstrap
+        initializeServices();
 
-		// Subscribe to window events from Services
-		setupEventSubscriptions();
+        // Set up frame callback to update services each frame
+        bootstrap->setFrameCallback([this]()
+        {
+            if (inputService)
+            {
+                inputService->update();
+            }
+            if (windowStateService)
+            {
+                windowStateService->update();
+            }
+        });
 
-		windowImguiHandler->init();
-	}
+        // Subscribe to window events from Services
+        setupEventSubscriptions();
 
-	void EditorHandler::run() const
-	{
-		bootstrap->run();
-	}
+        windowImguiHandler->init();
+    }
 
-	void EditorHandler::cleanUp()
-	{
-		cleanupEventSubscriptions();
+    void EditorHandler::run() const
+    {
+        bootstrap->run();
+    }
 
-		windowImguiHandler->cleanUp();
-		
-		previewService.reset();
-		renderService.reset();
-		sceneService.reset();
-		
-		audioSceneUpdater.reset();
-		audioService.reset();
-		editorModeService.reset();
-		windowStateService.reset();
-		inputService.reset();
+    void EditorHandler::cleanUp()
+    {
+        cleanupEventSubscriptions();
 
-		bootstrap->cleanUp();
-	}
+        windowImguiHandler->cleanUp();
 
-	void EditorHandler::initializeServices()
-	{
-		// Create service implementations using providers from bootstrap
-		sceneService = std::make_shared<services::SceneServiceImpl>(bootstrap->getSceneGraphSystem());
-		renderService = std::make_shared<services::EditorRenderServiceImpl>(
-			bootstrap->getOffScreenProvider(),
-			bootstrap->getEditorTextureProvider()
-		);
-		inputService = std::make_shared<services::InputServiceImpl>(bootstrap->getWindow());
-		windowStateService = std::make_shared<services::WindowStateServiceImpl>(bootstrap->getWindow());
-		previewService = std::make_shared<services::PreviewServiceImpl>(
-			bootstrap->getMaterialPreviewProvider(),
-			bootstrap->getMeshPreviewProvider()
-		);
-		editorModeService = std::make_shared<services::EditorModeServiceImpl>();
-		audioService = std::make_shared<services::AudioServiceImpl>(bootstrap->getAudioProvider());
-		audioSceneUpdater = std::make_unique<core::audio::AudioSceneUpdater>();
+        previewService.reset();
+        renderService.reset();
+        sceneService.reset();
 
-		// Register event handlers for command/query pattern
-		sceneService->registerEventHandlers();
-		renderService->registerEventHandlers();
-		inputService->registerEventHandlers();
-		windowStateService->registerEventHandlers();
-		previewService->registerEventHandlers();
-		editorModeService->registerEventHandlers();
-		static_cast<services::AudioServiceImpl*>(audioService.get())->registerEventHandlers();
+        audioSceneUpdater.reset();
+        audioService.reset();
+        editorModeService.reset();
+        windowStateService.reset();
+        inputService.reset();
 
-		events::render::LoadBillboardAtlasCommand atlasCmd;
-		atlasCmd.atlasPath = "../../resources/editor/billboardAtlas.vfImage";
-		events::EventDispatcher::instance().execute(atlasCmd);
-	}
+        bootstrap->cleanUp();
+    }
 
-	void EditorHandler::setupEventSubscriptions()
-	{
-		auto& dispatcher = events::EventDispatcher::instance();
-		
-		resizeSubscription = dispatcher.subscribe<events::application::WindowResizedNotification>(
-			[this](const events::application::WindowResizedNotification&) {
-				bootstrap->triggerResize();
-			});
-		
-		minimizeSubscription = dispatcher.subscribe<events::application::WindowMinimizedNotification>(
-			[](const events::application::WindowMinimizedNotification&) {
-				// Could pause rendering or other expensive operations here
-			});
+    void EditorHandler::initializeServices()
+    {
+        // Create service implementations using providers from bootstrap
+        sceneService = std::make_shared<services::SceneServiceImpl>(bootstrap->getSceneGraphSystem());
+        renderService = std::make_shared<services::EditorRenderServiceImpl>(
+            bootstrap->getOffScreenProvider(),
+            bootstrap->getEditorTextureProvider()
+        );
+        inputService = std::make_shared<services::InputServiceImpl>(bootstrap->getWindow());
+        windowStateService = std::make_shared<services::WindowStateServiceImpl>(bootstrap->getWindow());
+        previewService = std::make_shared<services::PreviewServiceImpl>(
+            bootstrap->getMaterialPreviewProvider(),
+            bootstrap->getMeshPreviewProvider()
+        );
+        editorModeService = std::make_shared<services::EditorModeServiceImpl>();
+        audioService = std::make_shared<services::AudioServiceImpl>(bootstrap->getAudioProvider());
+        audioSceneUpdater = std::make_unique<core::audio::AudioSceneUpdater>();
 
-		restoreSubscription = dispatcher.subscribe<events::application::WindowRestoredNotification>(
-			[](const events::application::WindowRestoredNotification&) {
-				// Could resume rendering or other operations here
-			});
-		
-		focusSubscription = dispatcher.subscribe<events::application::WindowFocusedNotification>(
-			[](const events::application::WindowFocusedNotification&) {
-				// Could handle focus changes (e.g., pause input when unfocused)
-			});
-	}
+        // Register event handlers for command/query pattern
+        sceneService->registerEventHandlers();
+        renderService->registerEventHandlers();
+        inputService->registerEventHandlers();
+        windowStateService->registerEventHandlers();
+        previewService->registerEventHandlers();
+        editorModeService->registerEventHandlers();
+        static_cast<services::AudioServiceImpl*>(audioService.get())->registerEventHandlers();
 
-	void EditorHandler::cleanupEventSubscriptions()
-	{
-		auto& dispatcher = events::EventDispatcher::instance();
+        events::render::LoadBillboardAtlasCommand atlasCmd;
+        atlasCmd.atlasPath = "../../resources/editor/billboardAtlas.vfImage";
+        events::EventDispatcher::instance().execute(atlasCmd);
+    }
 
-		if (resizeSubscription.isValid()) {
-			dispatcher.unsubscribe(resizeSubscription);
-			resizeSubscription = {};
-		}
-		if (minimizeSubscription.isValid()) {
-			dispatcher.unsubscribe(minimizeSubscription);
-			minimizeSubscription = {};
-		}
-		if (restoreSubscription.isValid()) {
-			dispatcher.unsubscribe(restoreSubscription);
-			restoreSubscription = {};
-		}
-		if (focusSubscription.isValid()) {
-			dispatcher.unsubscribe(focusSubscription);
-			focusSubscription = {};
-		}
-	}
+    void EditorHandler::setupEventSubscriptions()
+    {
+        auto& dispatcher = events::EventDispatcher::instance();
+
+        resizeSubscription = dispatcher.subscribe<events::application::WindowResizedNotification>(
+            [this](const events::application::WindowResizedNotification&)
+            {
+                bootstrap->triggerResize();
+            });
+
+        minimizeSubscription = dispatcher.subscribe<events::application::WindowMinimizedNotification>(
+            [](const events::application::WindowMinimizedNotification&)
+            {
+                // Could pause rendering or other expensive operations here
+            });
+
+        restoreSubscription = dispatcher.subscribe<events::application::WindowRestoredNotification>(
+            [](const events::application::WindowRestoredNotification&)
+            {
+                // Could resume rendering or other operations here
+            });
+
+        focusSubscription = dispatcher.subscribe<events::application::WindowFocusedNotification>(
+            [](const events::application::WindowFocusedNotification&)
+            {
+                // Could handle focus changes (e.g., pause input when unfocused)
+            });
+    }
+
+    void EditorHandler::cleanupEventSubscriptions()
+    {
+        auto& dispatcher = events::EventDispatcher::instance();
+
+        if (resizeSubscription.isValid())
+        {
+            dispatcher.unsubscribe(resizeSubscription);
+            resizeSubscription = {};
+        }
+        if (minimizeSubscription.isValid())
+        {
+            dispatcher.unsubscribe(minimizeSubscription);
+            minimizeSubscription = {};
+        }
+        if (restoreSubscription.isValid())
+        {
+            dispatcher.unsubscribe(restoreSubscription);
+            restoreSubscription = {};
+        }
+        if (focusSubscription.isValid())
+        {
+            dispatcher.unsubscribe(focusSubscription);
+            focusSubscription = {};
+        }
+    }
 }
