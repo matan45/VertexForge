303961b050e562c8c5529fb3d8fbb4efc8cebd1e
matan <matan33214@gmail.com>
2026-02-01 12:43:26 +0200

    more fix

--- VFEngine/editor/windows/ViewPortOverlay.cpp
+++ VFEngine/editor/windows/ViewPortOverlay.cpp
@@ -109,11 +109,18 @@ namespace windows
             currentViewMode = static_cast<int>(dispatcher.query(events::render::GetViewModeQuery{}));
 
             const char* viewModeLabels[] = {
-                "Color", "Meshlet", "Mipmap", "Cluster", "Depth", "Shadow",
-                "DAG Cluster", "DAG Level", "Streaming", "Terrain Edge"
+                "Color",        // 0: Normal rendering
+                "Meshlet",      // 1: Meshlet visualization
+                "LOD",          // 2: LOD level colors
+                "Mipmap",       // 3: Texture mip level
+                "Cluster",      // 4: Light cluster visualization
+                "Depth",        // 5: Depth visualization
+                "Shadow",       // 6: Shadow visualization
+                "Terrain Tile", // 7: Terrain tile visualization (color per tile)
+                "Terrain UV"    // 8: Terrain world UV visualization
             };
             ImGui::SetNextItemWidth(dropdownWidth);
-            if (ImGui::Combo("##ViewMode", &currentViewMode, viewModeLabels, 10))
+            if (ImGui::Combo("##ViewMode", &currentViewMode, viewModeLabels, 9))
             {
                 events::render::SetViewModeCommand cmd;
                 cmd.mode = static_cast<uint32_t>(currentViewMode);
